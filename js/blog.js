39f8c6e44183e7a5cc31d5a93bc25be5f87f15ca
chrisduffey <duffeyvt12@gmail.com>
2024-03-27 23:01:22 -0400

    javaScript

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,21 @@
-// selctors
+function displayBlogs(){
+    const containerEl = document.querySelector(".container")
+    containerEl.innerHTML = ""
+    const blogs = JSON.parse(localStorage.getItem("blogs")) || []
+    for(let i =0; i < blogs.length; i++){
+        const cardEl = document.createElement("div")
+        const titleEl = document.createElement("h3")
+        const contentEl = document.createElement("p")
+        const userEl = document.createElement("p")
+        titleEl.textContent = blogs[i].title
+        contentEl.textContent = blogs[i].content
+        userEl.textContent = `posted by: ${blogs[i].userName} `
+        cardEl.classList.add("card")
+        cardEl.append(titleEl, contentEl, userEl)
+        containerEl.append(cardEl)
+    }
+}
+displayBlogs()
 const themeToggleBtn =document.querySelector(`.dark-mode`)
 
 // state
@@ -23,5 +40,5 @@ themeToggleBtn.addEventListener(`click`, ( )=>{
 })
 
 document.getElementById("go-back").addEventListener("click", () =>{
-    history.back();
+    location.href="index.html";
 })
--- js/form.js
+++ js/form.js
@@ -1,5 +1,33 @@
-// selctors
+function handleSubmit (event) {
+    event.preventDefault()
+    const userEl = document.getElementById(`name`).value.trim()
+    const titleEL = document.getElementById(`title`).value.trim()
+    const contentEl = document.getElementById(`content`).value.trim()
+    if(!userEl || !titleEL || !contentEl){
+        const errorEl = document.getElementById("error")
+        errorEl.textContent = "Plese fill all info"
+        setTimeout(() => {
+         errorEl.textContent = ""   
+        }, 3000);
+        return;
+    }
+    const data = {
+        userName: userEl, title: titleEL, content: contentEl
+    }
+   handleLocalStorage(data)
+   location.href = "blog.html"
+}
+function handleLocalStorage(data) {
+    const blogs = JSON.parse(localStorage.getItem("blogs")) || []
+    blogs.push(data)
+    localStorage.setItem("blogs", JSON.stringify(blogs))
+}
+
+
 const themeToggleBtn =document.querySelector(`#dark-light`)
+
+console.log(`themeToggleBtn`)
+
 
 // state
 const theme = localStorage.getItem(`theme`);
@@ -11,13 +39,18 @@ document.body.classList.add(theme)
 
 // handlers
 
-themeToggleBtn.addEventListener(`click`, ( )=>{
-    document.body.classList.toggle(`dark-mode`);
-    if (document.body.classList.contains(`dark-mode`)){
 
-    localStorage.setItem(`theme`, `dark-mode`);
-    } else{
-        localStorage.removeItem(`theme`);
 
-    }
-})
+
+// themeToggleBtn.addEventListener(`click`, (event)=>{
+//     document.body.classList.toggle(`dark-mode`);
+//     if (document.body.classList.contains(`dark-mode`)){
+
+//     localStorage.setItem(`theme`, `dark-mode`);
+//     } else{
+//         localStorage.removeItem(`theme`);
+
+//     }
+// })
+
+document.querySelector("#content-form").addEventListener(`submit`, handleSubmit)
